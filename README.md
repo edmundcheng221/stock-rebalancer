17fd9aaaaeace496fc97781d4e6ff553463287eb
Edmund Cheng <ec3219@nyu.edu>
2025-12-25 17:07:12 -0500

    format fixes

--- README.md
+++ README.md
@@ -12,3 +12,15 @@ docker compose up
 Verify that server is running:
 
 > http://localhost:8080/_healthy
+
+## Fix Formatting/Linting
+
+Formatting:
+```bash
+cargo fmt
+```
+
+Linting:
+```bash
+cargo clippy --fix
+```
--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,24 @@
-use axum::{
-    routing::get,
-    Router,
-};
-use tokio::net::TcpListener;
+use axum::{Router, routing::get};
 use std::net::SocketAddr;
+use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
     tracing_subscriber::fmt::init();
-    
+
     let port = 8080;
 
     // health check
     let app = Router::new().route("/_healthy", get(health_check));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    tracing::info!("listening on {}", addr); 
+    tracing::info!("listening on {}", addr);
 
     let listener: TcpListener = TcpListener::bind(&addr)
         .await
         .expect("failed to bind address");
 
-    axum::serve(listener, app)
-        .await
-        .expect("server error")
-
+    axum::serve(listener, app).await.expect("server error")
 }
 
 // Healthy Check Probe
